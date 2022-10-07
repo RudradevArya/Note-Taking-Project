9bc2dd6c9580c1300bef6f48096eb29a17b4ce19
Rudradev Arya <71017449+RudradevArya@users.noreply.github.com>
2022-10-07 17:19:19 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Rudradev's First Note Taking App Using JS</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>
+        This is a note taking application made by Rudradev Arya using CSS &
+        React JS
+      </h1>
+      <p contentEditable="true"></p>
+    </div>
+  );
+}
+
+export default Notes;
